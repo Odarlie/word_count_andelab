6434da3de7e5ca4c440562dc4a3624ab2d97abf9
odarlie <odarlie@gmail.com>
2017-06-14 08:45:22 +0300

    I have added a loop that loops through the wordoccurence dictionary and prints the word and the number of times it occurs

--- word_count_andelab.py
+++ word_count_andelab.py
@@ -2,3 +2,6 @@ from collections import Counter
 def word_count(sentence):
     words = sentence.split(' ')
     wordoccurence = Counter(words)
+    for k,v in wordoccurence.items():
+        print('{0}: {1}'.format(k,v))
+        
